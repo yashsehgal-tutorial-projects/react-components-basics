472591d6f1982a6d91bd78218f1943b36451c9f3
yashsehgal <yashsehgal.work@gmail.com>
2021-08-06 02:08:31 +0530

    code sprint (1/3) added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/index.css';
+import React from 'react';
+import GetProfileData from './data/GetProfileData';
+import ProfileCard from './components/ProfileCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="gradient-text"
+          style={{
+            maxWidth: '24ch',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          Search for professional profiles from all around the world
+        </h1>
+        <div className="search-input-wrapper"
+          style={{
+            width: 'fit-content',
+            display: 'flex',
+            alignItems: 'stretch',
+            gap: '4px'
+          }}
+        >
+          <input type="text" 
+            name="search-value"
+            className="search-profile-input-box"
+            id="search-profile-input"
+            placeholder="Search People, Companies and Jobs"
+            style={{
+              width: '420px'
+            }}
+          />
+          <button
+            className="primary-button button__search"
+            id="search-button"
+            style={{
 
-export default App;
+            }}
+          >
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
+
+        <div className="profile-card-container">
+          <h2>People you might want to connect with</h2>
+          {
+            GetProfileData('home').map((profileData, index) => {
+              return (
+                <ProfileCard 
+                  BannerImage={profileData.banner_image}
+                  ProfileDisplay={profileData.profile_photo}
+                  Name={profileData.name}
+                  Username={profileData.username}
+                  Bio={profileData.bio}
+                  UserType={profileData.user_type}
+                  Followers={profileData.followers}
+                  Connections={profileData.connections}
+                  posts={profileData.posts}
+                />
+              )
+            })
+          }
+        </div>
+        
+      </div>
+    )
+  }
+}
---
+++ src/data/GetProfileData.js
@@ -0,0 +1,65 @@
+
+export default function GetProfileData(type) {
+  const home_view_profile_data = {
+    "home_view_profile_data": [
+      {
+        "banner_image": "https://github.com/yashsehgal/yashsehgal/raw/main/banner.png",
+        "profile_photo": "https://avatars.githubusercontent.com/u/62352288?v=4",
+        "name": "Yash Sehgal",
+        "username": "sehgalyash",
+        "bio": "Founder @DesignSystemsOSS | Frontend Intern @Economize Open Source Contributor | Mentor at Hackathons",
+        "user_type": "working",
+        "followers": "2347",
+        "connections": "500+",
+        "posts": "1179"
+      },
+      {
+        "banner_image": "https://github.com/yashsehgal/yashsehgal/raw/main/banner.png",
+        "profile_photo": "https://avatars.githubusercontent.com/u/62352288?v=4",
+        "name": "Yash Sehgal",
+        "username": "sehgalyash",
+        "bio": "Founder @DesignSystemsOSS | Frontend Intern @Economize Open Source Contributor | Mentor at Hackathons",
+        "user_type": "working",
+        "followers": "2347",
+        "connections": "500+",
+        "posts": "1179"
+      },
+      {
+        "banner_image": "https://github.com/yashsehgal/yashsehgal/raw/main/banner.png",
+        "profile_photo": "https://avatars.githubusercontent.com/u/62352288?v=4",
+        "name": "Yash Sehgal",
+        "username": "sehgalyash",
+        "bio": "Founder @DesignSystemsOSS | Frontend Intern @Economize Open Source Contributor | Mentor at Hackathons",
+        "user_type": "working",
+        "followers": "2347",
+        "connections": "500+",
+        "posts": "1179"
+      },
+      {
+        "banner_image": "https://github.com/yashsehgal/yashsehgal/raw/main/banner.png",
+        "profile_photo": "https://avatars.githubusercontent.com/u/62352288?v=4",
+        "name": "Yash Sehgal",
+        "username": "sehgalyash",
+        "bio": "Founder @DesignSystemsOSS | Frontend Intern @Economize Open Source Contributor | Mentor at Hackathons",
+        "user_type": "working",
+        "followers": "2347",
+        "connections": "500+",
+        "posts": "1179"
+      },
+      {
+        "banner_image": "https://github.com/yashsehgal/yashsehgal/raw/main/banner.png",
+        "profile_photo": "https://avatars.githubusercontent.com/u/62352288?v=4",
+        "name": "Yash Sehgal",
+        "username": "sehgalyash",
+        "bio": "Founder @DesignSystemsOSS | Frontend Intern @Economize Open Source Contributor | Mentor at Hackathons",
+        "user_type": "working",
+        "followers": "2347",
+        "connections": "500+",
+        "posts": "1179"
+      }
+    ]
+  }
+  if (type === 'home') {
+    return home_view_profile_data.home_view_profile_data;
+  }
+}
